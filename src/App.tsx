a52d9ba359eab0451ea558b746274448cc176636
NicuV23 <v.nicu213@gmail.com>
2024-02-12 23:49:26 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+interface questions {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+const FlashCards: React.FC = () => {
+  const [selectedID, setSelectedID] = useState<number | null>(null);
+
+  return (
+    <>
+      <div className="flashcards">
+        {questions.map((item) => (
+          <div
+            onClick={() => setSelectedID(item.id)}
+            key={item.id}
+            className={item.id === selectedID ? "selected" : ""}
+          >
+            {item.id === selectedID ? item.answer : item.question}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
